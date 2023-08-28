7501925afca93c213bccc4f3204cd4293e4994b7
Akhilbaby77 <141495006+Akhilbaby77@users.noreply.github.com>
2023-08-28 09:51:45 +0530

    Add files via upload

---
+++ XML-XSL-Webstack--master/product.py
@@ -0,0 +1,28 @@
+from lxml import etree
+
+# Load XML and XSL files
+xml = etree.parse("products.xml")
+xsl = etree.parse("transform.xsl")
+xsd = etree.parse("product_schema.xsd")
+
+# Apply XSL transformationq
+transform = etree.XSLT(xsl)
+html = transform(xml)
+print(html)
+
+# Save transformed HTML to a file
+with open('product.html', 'wb') as output_file:
+    output_file.write(etree.tostring(html, pretty_print=True))
+
+# Load XSD schema
+schema = etree.XMLSchema(xsd)
+
+# Validate transformed HTML against XSD schema
+validation_result = schema.validate(xml)
+
+if validation_result:
+    print("Validation successful!")
+else:
+    print("Validation errors:")
+    print("Validation Result :",validation_result)
+    print(schema.error_log)
